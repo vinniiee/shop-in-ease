9875296df9b5775725ed35ae56eba19c48f90b25
vinniiee <vs28245@gmail.com>
2025-07-29 08:14:45 +0530

    fix home redirect

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-if (currentUser != null) {
-  location.href = "/shop";
-}
-
-if (!localStorage.getItem("cart")) {
-  localStorage.setItem("cart", JSON.stringify([]));
-}
-
 const basePath =
   window.location.hostname === "127.0.0.1" ||
   window.location.hostname === "localhost"
     ? ""
     : "/shop-in-ease";
 
+if (currentUser != null) {
+  location.href = basePath+"/shop";
+}
+
+if (!localStorage.getItem("cart")) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+
+
 if (basePath !== "") {
   document.querySelectorAll("a").forEach((link) => {
     link.setAttribute("href", basePath + link.getAttribute("href"));
   });
 }
+
+
